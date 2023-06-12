78d32f87a49df99aa4570220a89546d7c5a3eb03
Ranaujjawal <105837804+Ranaujjawal@users.noreply.github.com>
2023-06-13 00:48:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://campusdiarypublic.onrender.com',
     methods: ['GET', 'POST'],
   },
 });
